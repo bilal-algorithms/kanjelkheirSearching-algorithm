4ba76a3bbd28716c337141f72d2704552804ac1c
Bilal <bilal.kanjelkheir@gmail.com>
2025-03-05 16:07:36 +0200

    Update main.cpp for taking input

--- main.cpp
+++ main.cpp
@@ -6,16 +6,20 @@ bool Even(int n);
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int k;
-    cin >> k;
-    vector<int> vec = {1,2,3,4,5,6,7,8,9,10};
-    int mid = vec.size() / 2;
+    int k, t;
+    cin >> t >> k;
+    vector<int> vec(t);
+    for (int i = 0; i < t; i++)
+    {
+        cin >> vec[i];
+    }
+    int mid = t / 2;
     if (vec[mid - 1] == k) {
         cout << "Found at index: " << mid << '\n';
         return 0;
     }
-    if (Even(vec.size())) {
-        for (int i = 0, j = (vec.size() - 1); i < mid, j > mid; i++, j--) {
+    if (Even(t)) {
+        for (int i = 0, j = (t - 1); i < mid, j > mid; i++, j--) {
             if (vec[i] == k) {
                 cout << "Found at index: " << i << '\n';
                 return 0;
@@ -25,7 +29,7 @@ int main() {
             }
         }
     } else {
-        for (int i = 1, j = (vec.size() - 1); i < mid, j > mid; i++, j--) {
+        for (int i = 1, j = (t - 1); i < mid, j > mid; i++, j--) {
             if (vec[i] == k) {
                 cout << "Found at index: " << i << '\n';
                 return 0;
